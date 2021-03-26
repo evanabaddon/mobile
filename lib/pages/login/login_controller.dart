fcaae9ea824079bbd5bd089513f13bc2fc299005
Evan Helga <34735813+evanabaddon@users.noreply.github.com>
2021-03-26 07:06:14 +0700

    login method nik / email

--- lib/pages/login/login_controller.dart
+++ lib/pages/login/login_controller.dart
@@ -25,4 +25,32 @@ class LoginController extends GetxController {
     pinController?.dispose();
     super.onClose();
   }
+
+  int which = 0;
+
+  void checktext(String text) {
+    if (text.isNum) {
+      print("${text} is a number");
+      which = 1;
+    } else if (text.isEmail) {
+      print("${text} is Email");
+      which = 2;
+    } else if (text.isEmpty) {
+      which = 0;
+    }
+
+    update();
+  }
+
+  void login() {
+    if (which == 1) {
+      //auth
+      print("Run a method from phone NIK signIn");
+    } else if (which == 2) {
+      print("Run a method from email signIn");
+      //auth
+    } else {
+      print("Show a message to user if textfield is empty");
+    }
+  }
 }
